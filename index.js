5cd32c92f113427ef89a8c88475590550af30d8d
Viktor Elofsson <viktor@viktorelofsson.se>
2019-11-10 15:07:00 +0100

    Update for Porla 0.14

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const request = require('request');
 const split = require('split');
-const unzip = require('unzip');
-const { Plugin, Porla } = require('@porla/porla');
+const unzipper = require('unzipper');
 
-const url = 'http://upd.emule-security.org/ipfilter.zip';
+const DEFAULT_URL = 'http://upd.emule-security.org/ipfilter.zip';
 
-async function applyBlocklist(session) {
+function applyBlocklist(url, { log }, session) {
     return new Promise((resolve, reject) => {
+        log.info('Downloading blocklist from %s', url);
+
+        // TODO: save zip file to state directory
+
         request(url)
-            .pipe(unzip.Parse())
+            .pipe(unzipper.Parse())
             .on('entry', (entry) => {
                 if (entry.path !== 'guarding.p2p') {
                     return entry.autodrain();
                 }
 
                 const filter = session.get_ip_filter();
+                let count = 0;
 
                 entry
                     .pipe(split())
                     .on('end', () => {
                         session.set_ip_filter(filter);
+                        log.info('Added %d address(es) to IP filter', count);
                         resolve();
                     })
                     .on('error', (err) => reject(err))
@@ -32,21 +37,24 @@ async function applyBlocklist(session) {
                         const to = d.substr(18, 15).replace(/\b0+\B/g, '');
 
                         filter.add_rule(from, to, 1);
+
+                        count += 1;
                     });
             });
     });
 }
 
-class Blocklist extends Plugin {
-    /**
-     * 
-     * @param {Porla} porla
-     */
-    load(porla) {
-        porla.subscribe('session.init', [
-            (session) => applyBlocklist(session)
-        ]);
+function blocklist(options) {
+    options = options || {};
+
+    return function(porla) {
+        porla.on('session.init', async ({ session }) => {
+            await applyBlocklist(
+                options.url || DEFAULT_URL,
+                porla,
+                session.native());
+        });
     }
 }
 
-module.exports = Blocklist;
+module.exports = blocklist;
